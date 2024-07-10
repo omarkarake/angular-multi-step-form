051c867d3209ee9d7835c0a1c5c591ad3603958b
omar karake <omar.karake@amalitechtraining.org>
2024-07-10 14:29:40 +0200

    components creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#EFF5FF",
+        "border-color": "#D6D9E6",
+        denim: "#022959",
+        grey: "#9699AA",
+        "light-blue": "#ABBCFF",
+        "light-grey": "#D6D9E6",
+        orange: "#FFAF7E",
+        pink: "#F9818E",
+        purple: "#483EFF",
+        "red-errors": "#EE374A",
+        "sky-blue": "#BEE2FD",
+        "very-light-gray": "#F8F9FF",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
