d60d73a4fd8847532c59c68a6a8614993b07605c
omar karake <omar.karake@amalitechtraining.org>
2024-07-11 09:41:03 +0200

    morbile about adon page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavigationComponent } from './pages/navigation/navigation.component';
 import { SelectPlanComponent } from './pages/select-plan/select-plan.component';
+import { PickAddOnsComponent } from './pick-add-ons/pick-add-ons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
-    SelectPlanComponent
+    SelectPlanComponent,
+    PickAddOnsComponent
   ],
   imports: [
     BrowserModule,
