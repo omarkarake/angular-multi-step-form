872fdde81d359276fb2cc6672b8df36f11d709bf
omar karake <omar.karake@amalitechtraining.org>
2024-07-13 12:57:44 +0200

    log the current duration as string

--- src/app/pages/pick-add-ons/pick-add-ons.component.ts
+++ src/app/pages/pick-add-ons/pick-add-ons.component.ts
@@ -1,24 +1,46 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pick-add-ons',
   templateUrl: './pick-add-ons.component.html',
   styleUrls: ['./pick-add-ons.component.css'],
 })
-export class PickAddOnsComponent {
+export class PickAddOnsComponent implements OnInit {
   @Input('currentStep') currentStep!: number;
+  @Input() selectedPlan: { plan: string; price: string } | null = null;
   @Output() next = new EventEmitter<void>();
   @Output() back = new EventEmitter<void>();
 
+  ngOnInit(): void {
+    console.log("selected plan duration", this.selectedPlan?.price.slice(-2));
+  }
   addOns = [
-    { name: 'Online service', description: 'Access to multiplayer games', price: '+$1/mo', selected: false },
-    { name: 'Larger storage', description: 'Extra 1TB of cloud save', price: '+$2/mo', selected: false },
-    { name: 'Customizable profile', description: 'Custom theme on your profile', price: '+$2/mo', selected: false }
+    {
+      name: 'Online service',
+      description: 'Access to multiplayer games',
+      price: '+$1/mo',
+      selected: false,
+    },
+    {
+      name: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      price: '+$2/mo',
+      selected: false,
+    },
+    {
+      name: 'Customizable profile',
+      description: 'Custom theme on your profile',
+      price: '+$2/mo',
+      selected: false,
+    },
   ];
 
   nextStep(event: Event) {
     event.stopPropagation();
-    console.log('Selected Add-Ons:', this.addOns.filter(addOn => addOn.selected));
+    console.log(
+      'Selected Add-Ons:',
+      this.addOns.filter((addOn) => addOn.selected)
+    );
     this.next.emit();
   }
 
