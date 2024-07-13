4746e18780536987853f1d286c808501bbea3f11
omar karake <omar.karake@amalitechtraining.org>
2024-07-13 16:44:10 +0200

    rendering total price on finishing up page

--- src/app/pages/finishing-up/finishing-up.component.ts
+++ src/app/pages/finishing-up/finishing-up.component.ts
@@ -1,6 +1,11 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-
+interface AddOn {
+  description: string;
+  name: string;
+  price: string;
+  selected: boolean;
+}
 @Component({
   selector: 'app-finishing-up',
   templateUrl: './finishing-up.component.html',
@@ -13,11 +18,20 @@ export class FinishingUpComponent implements OnInit {
   @Output() back = new EventEmitter<void>();
   formValue: any;
   durationString: any;
-  price:any;
+  price: any = [];
+  totalPrice: number = 0;
   ngOnInit(): void {
     this.formValue = this.form.value;
     console.log('initial render on finishing up: ', this.formValue);
-    console.log('selectedPrice: ', typeof Number(this.formValue.planSelection.price.match(/\d+/g).join("")));
+    this.price.push(
+      Number(this.formValue.planSelection.price.match(/\d+/g).join(''))
+    );
+    // console.log('selectedPriceAddons: ', this.formValue.addOns);
+    this.formValue.addOns.forEach((element: AddOn | any) => {
+      this.price.push(Number(element.price.match(/\d+/g).join('')));
+    });
+    this.totalPrice = this.price.reduce((a:number, b:number) => a + b);
+
     this.formValue.planSelection.price.slice(-2) === 'mo'
       ? (this.durationString = 'month')
       : (this.durationString = 'year');
