88e4e3bcd923f5c45325caed0876876acb5fbf03
omar karake <omar.karake@amalitechtraining.org>
2024-07-13 15:35:22 +0200

    getting the formGroup in the finishing up page

--- src/app/pages/finishing-up/finishing-up.component.ts
+++ src/app/pages/finishing-up/finishing-up.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-finishing-up',
   templateUrl: './finishing-up.component.html',
   styleUrl: './finishing-up.component.css',
 })
-export class FinishingUpComponent {
+export class FinishingUpComponent implements OnInit {
   @Input('currentStep') currentStep!: number;
+  @Input() form!: FormGroup;
   @Output() next = new EventEmitter<void>();
   @Output() back = new EventEmitter<void>();
-
+  formValue: object[] | null = null;
+  ngOnInit(): void {
+    this.formValue = this.form.value;
+    console.log("initial render on finishing up: ",this.formValue);
+  }
   nextStep(event: Event) {
     event.stopPropagation();
     this.next.emit();
